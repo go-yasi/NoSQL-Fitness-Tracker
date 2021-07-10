293adc02f4a638c532b86a95533aa636ab15b4d2
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-10 16:42:06 -0700

    Added WorkoutSchema to workoutdb.js file in models folder

--- models/workoutDB.js
+++ models/workoutDB.js
@@ -1,3 +1,45 @@
 const mongoose = require("mongoose");
-const Scheme = mongoose.Schema;
+const Schema = mongoose.Schema;
 
+const WorkoutSchema = new Schema({
+    exercise: [{
+        name: {
+            type: String,
+            trim: true,
+            required:"Workout name is required."
+        },
+        type: {
+            type: String,
+            trim: true,
+            required:"Workout type is required."
+        },
+        weight: {
+            type: Number,
+            required: true
+        },
+        sets: {
+            type: Number,
+            required: true
+        },
+        reps: {
+            type: Number,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        distance: {
+            type: Number,
+            required: false
+        }
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;

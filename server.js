03cad6857de1ad122c14ff579396a0da806a2668
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-10 17:25:20 -0700

    Added first post route to server.js to create new workout

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+const Workout = require("./models/workout");
 
 const app = express();
 
@@ -17,21 +17,31 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to mongoose DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // ### You will have 5 api routes to complete.
-// ​
+
 // A POST route to create a workout
-// ​
+app.post("api/workout", ({body}, res) => {
+    db.Workout.create(body) 
+    .then(({_id}) => db.Workout.findOneandUpdate({}, {$push:{ workout: _id }}, { new: true }))
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
 //  A PUT route to update a workout(
-    // HINT:you will have to find the workout by id and then push exercises to the exercises array)
-// ​
+// HINT:you will have to find the workout by id and then push exercises to the exercises array)
+
 //  A GET route to get the workouts
 // (HINT: this will need an aggregate to add all the durations from each exercise together. Here is an example https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/)
-// ​
+
 //   A GET route to get workouts in a specific range
 // (HINT:very similar to the one above, but needs a limit. Here is an exampe https://kb.objectrocket.com/mongo-db/how-to-use-the-mongoose-limit-function-927)
-// ​
+
 //   A DELETE route to delete a workout by a specific id
 
 app.listen(PORT, () => {

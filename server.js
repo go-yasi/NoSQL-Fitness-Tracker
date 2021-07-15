8929dda5f14cf9270bc41947aba1fc1ac7a4693e
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-14 17:22:16 -0700

    Deploying

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to mongoose DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 // need this to deploy to heroku
 app.get("/", function(req, res) {
